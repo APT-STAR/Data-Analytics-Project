bcbfb115e017bb6d3a880f62905ce724b6056d6f
APT-STAR <ayushtripathi218@gmail.com>
2025-03-30 02:39:27 +0530

    Data file

--- DataAnalytics.py
+++ DataAnalytics.py
@@ -1,4 +1,6 @@
-
-import pandas as pd
-import kaggle
-!kaggle datasets download ankitbansal06/retail-orders -f orders.csv
+''' In this particular project we are downloading an data set from kaggle api , then using pyhton reading the file 
+and the making the required chages like deleting the unwanted data dealing with none values and after that uploading 
+the data to an sql server and handling it 
+libraries used : Pnadas ,mssqlchemy 
+Api used : Kaggle
+'''
